1aa37569a2d29992f79d6441cec866757f1028ba
Max Herrera <harvey.herrera@lumation.co>
2022-09-24 12:18:11 -0600

    LocationModel is created to de-encode the coordinates and display them on the map on the MapView screen, using the MapKit framework.

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,50 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+
+        return mapRegion
+    } ()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Map View")
+        // MARK: - No. 1 BASIC MAP
+        // Map(coordinateRegion: $region)
+        
+        // MARK: - No. 2 ADVANCE MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: {
+            item in
+            
+            // (A) PIN: OLD STYLE (ALWAYS STATIC)
+            // MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) MARKER: NEW STYLE (ALWAYS STATIC)
+            // MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            } //: ANNOTATION
+        })
     }
 }
-
+// MARK: - PREVIEW
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Max Herrera on 9/24/22.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+}
