d3e350911a60d48a6b68103b8481f7a6a719e25a
Max Herrera <harvey.herrera@lumation.co>
2022-09-25 23:39:44 -0600

    Added custom grid for the gallery screen and interface interaction to select the current animal, this completes the gallery screen.

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK: - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - SIMPLE GRID DEFINITION
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    // MARK: - EFFICIENT GRID DEFINITION
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+  
+    // MARK: - DYNAMIC GRID DEFINITION
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumns: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumns))
+    }
+    
     var body: some View {
         
         ScrollView(.vertical, showsIndicators: false) {
-//            Text("Gallery View")
-        }
+            
+            
+            // MARK: - GRID
+            VStack(alignment: .center, spacing: 30) {
+                
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+
+                // MARK: - SLIDER
+                Slider(value: $gridColumns, in: 2...4 , step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumns
+                              , perform: { value in
+                        gridSwitch()
+                    })
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: LOOP
+                }//: GRID
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+        } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
         
