c22e062b5b3e036ef566eeab4178c7d5ff303bab
Max Herrera <harvey.herrera@lumation.co>
2022-09-26 00:17:26 -0600

    Grid advance view is added to show the list of animals in different views based on the main grid of the browse screen.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -10,27 +10,95 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: - PROPERTIES
-    
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive = false
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible()) ]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    // MARK: - FUNCTIONS
+    func gridSwitch () {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1 )
+        gridColumn = gridLayout.count
+        
+        // TOOLBAR IMAGE
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+        
+    }
+    
+    
     
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                
-                // MARK: - SLIDER
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                // MARK: - LIST ANIMALS
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
+            
+            Group {
+                if !isGridViewActive {
+                    List {
+                        // MARK: - SLIDER
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        // MARK: - LIST ANIMALS
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        } //: LOOP
+                    }
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { item in
+                                NavigationLink(destination: AnimalDetailView(animal: item)) {
+                                    AnimalGridItemView(animal: item)
+                                } //: LINK
+                            } //: LOOP
+                        } //: GRID
+                        .padding(10)
+                    } //: SCROLL
+                } //: LIST
+            } //: CONDITIONAL
+            .navigationTitle("Africa")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // MARK: - LIST
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        // MARK: - GRID
+                        Button(action: {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                            gridSwitch()
+                        }) {
+                            Image(systemName: toolbarIcon)
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+
                     }
                 }
             }
-            .navigationTitle("Africa")
-            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,34 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Max Herrera on 9/25/22.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+// MARK: - PREVIEW
+struct AnimalGridItemView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[1])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
